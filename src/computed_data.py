7dae9c202bfc87b919b4e81a8e424c94c93ac46b
eprbell <77937475+eprbell@users.noreply.github.com>
2021-08-24 11:13:21 -0700

    Made all dataclasses hashable, with frozen=True and eq=True. Added custom hash and comparison functions to YearlyGainLoss. Made input_data a normal var, instead of an InitVar and updated in/out/intra_transaction_set properties to go through input_data.

--- src/computed_data.py
+++ src/computed_data.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import InitVar, dataclass
+from dataclasses import dataclass
 from decimal import Decimal
 from typing import List
 
@@ -25,12 +25,12 @@ from rp2_error import RP2TypeError, RP2ValueError
 from transaction_set import TransactionSet
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, eq=True)
 class YearlyGainLoss:
     year: int
     asset: str
     transaction_type: TransactionType
-    is_long_term: bool
+    is_long_term_capital_gains: bool
     crypto_amount: Decimal
     usd_amount: Decimal
     usd_cost_basis: Decimal
@@ -40,17 +40,50 @@ class YearlyGainLoss:
         Configuration.type_check_positive_int("year", self.year)
         Configuration.type_check_string("asset", self.asset)
         TransactionType.type_check("transaction_type", self.transaction_type)
-        Configuration.type_check_bool("is_long_term", self.is_long_term)
+        Configuration.type_check_bool("is_long_term", self.is_long_term_capital_gains)
         Configuration.type_check_decimal("crypto_amount", self.crypto_amount)
         Configuration.type_check_decimal("usd_amount", self.usd_amount)
         Configuration.type_check_decimal("usd_cost_basis", self.usd_cost_basis)
         Configuration.type_check_decimal("usd_gain_loss", self.usd_gain_loss)
 
-    def __lt__(self, other: "YearlyGainLoss") -> bool:
+    def __eq__(self, other: object) -> bool:
+        if not other:
+            return False
+        if not isinstance(other, YearlyGainLoss):
+            raise RP2TypeError(f"Operand has non-YearlyGainLoss value {repr(other)}")
+        result: bool = self.year == other.year and self.asset == other.asset and self.transaction_type == other.transaction_type and self.is_long_term_capital_gains == other.is_long_term_capital_gains
+        return result
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+    def __lt__(self, other: object) -> bool:
+        if not other:
+            return False
+        if not isinstance(other, YearlyGainLoss):
+            raise RP2TypeError(f"Operand has non-YearlyGainLoss value {repr(other)}")
         return self.year < other.year
 
+    def __gt__(self, other: object) -> bool:
+        if not other:
+            return False
+        if not isinstance(other, YearlyGainLoss):
+            raise RP2TypeError(f"Operand has non-YearlyGainLoss value {repr(other)}")
+        return self.year > other.year
 
-@dataclass
+    def __ge__(self, other: object) -> bool:
+        return not self.__lt__(other)
+
+    def __le__(self, other: object) -> bool:
+        return not self.__gt__(other)
+
+    def __hash__(self) -> int:
+        # By definition, unique_id can uniquely identify a transaction: this works even if it's the ODS line from the spreadsheet,
+        # since there are no cross-asset transactions (so a spreadsheet line points to a unique transaction for that asset).
+        return hash((self.year, self.asset, self.transaction_type, self.is_long_term_capital_gains))
+
+
+@dataclass(frozen=True, eq=True)
 class ComputedData:
     asset: str
     taxable_event_set: TransactionSet
@@ -58,7 +91,7 @@ class ComputedData:
     balance_set: BalanceSet
     yearly_gain_loss_list: List[YearlyGainLoss]
     price_per_unit: Decimal
-    input_data: InitVar[InputData]
+    input_data: InputData
 
     @classmethod
     def type_check(cls, name: str, instance: "ComputedData") -> "ComputedData":
@@ -67,19 +100,15 @@ class ComputedData:
             raise RP2TypeError(f"Parameter '{name}' is not of type {cls.__name__}: {instance}")
         return instance
 
-    def __post_init__(self, input_data: InputData) -> None:
+    def __post_init__(self) -> None:
         Configuration.type_check_string("asset", self.asset)
         TransactionSet.type_check("taxable_event_set", self.taxable_event_set, EntrySetType.MIXED, self.asset, True)
         GainLossSet.type_check("gain_loss_set", self.gain_loss_set)
         BalanceSet.type_check("balance_set", self.balance_set)
         if not isinstance(self.yearly_gain_loss_list, List):
             raise RP2TypeError(f"Parameter 'yearly_gain_loss_list' is not of type List: {self.yearly_gain_loss_list}")
-        InputData.type_check("input_data", input_data)
         Configuration.type_check_positive_decimal("price_per_unit", self.price_per_unit)
-
-        self.__in_transaction_set: TransactionSet = input_data.in_transaction_set
-        self.__out_transaction_set: TransactionSet = input_data.out_transaction_set
-        self.__intra_transaction_set: TransactionSet = input_data.intra_transaction_set
+        InputData.type_check("input_data", self.input_data)
 
         if self.taxable_event_set.asset != self.asset:
             raise RP2ValueError(f"Asset mismatch in 'taxable_event_set': expected {self.asset}, found {self.taxable_event_set.asset}")
@@ -88,17 +117,17 @@ class ComputedData:
         if self.balance_set.asset != self.asset:
             raise RP2ValueError(f"Asset mismatch in 'balance_set': expected {self.asset}, found {self.balance_set.asset}")
 
-        if self.asset != input_data.asset:
-            raise RP2ValueError(f"Asset mismatch in 'input_data': expected {self.asset}, found {input_data.asset}")
+        if self.asset != self.input_data.asset:
+            raise RP2ValueError(f"Asset mismatch in 'input_data': expected {self.asset}, found {self.input_data.asset}")
 
     @property
     def in_transaction_set(self) -> TransactionSet:
-        return self.__in_transaction_set
+        return self.input_data.in_transaction_set
 
     @property
     def out_transaction_set(self) -> TransactionSet:
-        return self.__out_transaction_set
+        return self.input_data.out_transaction_set
 
     @property
     def intra_transaction_set(self) -> TransactionSet:
-        return self.__intra_transaction_set
+        return self.input_data.intra_transaction_set
