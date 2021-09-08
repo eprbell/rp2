b1f46700d8e77e3c0d961d3eb3dffd7a9b7b4710
eprbell <77937475+eprbell@users.noreply.github.com>
2021-09-08 11:08:51 -0700

    Bump version: 0.5.0 → 0.5.1

--- src/rp2/configuration.py
+++ src/rp2/configuration.py
@@ -25,7 +25,7 @@ from rp2.configuration_schema import CONFIGURATION_SCHEMA
 from rp2.rp2_decimal import RP2Decimal, ZERO
 from rp2.rp2_error import RP2TypeError, RP2ValueError
 
-VERSION: str = "0.5.0"
+VERSION: str = "0.5.1"
 
 
 class AccountingMethod(Enum):
