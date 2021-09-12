be5848d8683a1dce9aaa50c50e68d0e40feb2ff3
eprbell <77937475+eprbell@users.noreply.github.com>
2021-09-12 15:48:31 -0700

    Add pylint: disable=C0103. Fix non-PEP-8 variable names

--- tests/test_rp2_decimal.py
+++ tests/test_rp2_decimal.py
@@ -20,18 +20,18 @@ from rp2.rp2_error import RP2TypeError
 
 class TestRP2Decimal(unittest.TestCase):
     def setUp(self) -> None:
-        self.maxDiff = None
+        self.maxDiff = None  # pylint: disable=C0103
 
     def test_rp2_decimal(self) -> None:
-        n1: RP2Decimal = RP2Decimal("0.2")
-        n2: RP2Decimal = RP2Decimal("0.2")
-        n3: RP2Decimal = RP2Decimal("0.20000000000001")
-        n4: RP2Decimal = RP2Decimal("0.2000000000001")
+        decimal1: RP2Decimal = RP2Decimal("0.2")
+        decimal2: RP2Decimal = RP2Decimal("0.2")
+        decimal3: RP2Decimal = RP2Decimal("0.20000000000001")
+        decimal4: RP2Decimal = RP2Decimal("0.2000000000001")
 
-        n11: RP2Decimal = RP2Decimal("0.1")
-        n12: RP2Decimal = RP2Decimal("0.1")
-        n13: RP2Decimal = RP2Decimal("0.10000000000001")
-        n14: RP2Decimal = RP2Decimal("0.1000000000001")
+        decimal5: RP2Decimal = RP2Decimal("0.1")
+        decimal6: RP2Decimal = RP2Decimal("0.1")
+        decimal7: RP2Decimal = RP2Decimal("0.10000000000001")
+        decimal8: RP2Decimal = RP2Decimal("0.1000000000001")
 
         zero_point_twelve = RP2Decimal("0.12")
         one: RP2Decimal = RP2Decimal("1")
@@ -39,67 +39,66 @@ class TestRP2Decimal(unittest.TestCase):
         three: RP2Decimal = RP2Decimal("3")
         nine: RP2Decimal = RP2Decimal("9")
 
-        self.assertTrue(n1 == n2)
-        self.assertTrue(n1 == n3)
-        self.assertFalse(n1 == n4)
+        self.assertTrue(decimal1 == decimal2)
+        self.assertTrue(decimal1 == decimal3)
+        self.assertFalse(decimal1 == decimal4)
 
-        self.assertFalse(n1 != n2)
-        self.assertFalse(n1 != n3)
-        self.assertTrue(n1 != n4)
+        self.assertFalse(decimal1 != decimal2)
+        self.assertFalse(decimal1 != decimal3)
+        self.assertTrue(decimal1 != decimal4)
 
-        self.assertFalse(n1 > n2)
-        self.assertFalse(n1 > n3)
-        self.assertFalse(n1 > n4)
+        self.assertFalse(decimal1 > decimal2)
+        self.assertFalse(decimal1 > decimal3)
+        self.assertFalse(decimal1 > decimal4)
 
-        self.assertTrue(n1 >= n2)
-        self.assertTrue(n1 >= n3)
-        self.assertFalse(n1 >= n4)
+        self.assertTrue(decimal1 >= decimal2)
+        self.assertTrue(decimal1 >= decimal3)
+        self.assertFalse(decimal1 >= decimal4)
 
-        self.assertFalse(n1 < n2)
-        self.assertFalse(n1 < n3)
-        self.assertTrue(n1 < n4)
+        self.assertFalse(decimal1 < decimal2)
+        self.assertFalse(decimal1 < decimal3)
+        self.assertTrue(decimal1 < decimal4)
 
-        self.assertTrue(n1 <= n2)
-        self.assertTrue(n1 <= n3)
-        self.assertTrue(n1 <= n4)
+        self.assertTrue(decimal1 <= decimal2)
+        self.assertTrue(decimal1 <= decimal3)
+        self.assertTrue(decimal1 <= decimal4)
 
         # Check that overridden operators return RP2Decimal (these tests would fail without the custom precision built into RP2Decimal comparison operators)
-        self.assertTrue(n11 + n12 == n1)
-        self.assertTrue(n12 + n11 == n1)
-        self.assertTrue(n11 + n13 == n1)
-        self.assertTrue(n13 + n11 == n1)
-        self.assertFalse(n11 + n14 == n1)
-        self.assertFalse(n14 + n11 == n1)
-        self.assertTrue(n11 + n13 == n11 + n12)
-        self.assertFalse(n11 + n14 == n11 + n12)
+        self.assertTrue(decimal5 + decimal6 == decimal1)
+        self.assertTrue(decimal6 + decimal5 == decimal1)
+        self.assertTrue(decimal5 + decimal7 == decimal1)
+        self.assertTrue(decimal7 + decimal5 == decimal1)
+        self.assertFalse(decimal5 + decimal8 == decimal1)
+        self.assertFalse(decimal8 + decimal5 == decimal1)
+        self.assertTrue(decimal5 + decimal7 == decimal5 + decimal6)
+        self.assertFalse(decimal5 + decimal8 == decimal5 + decimal6)
 
-        self.assertTrue(n1 - n2 == ZERO)
-        self.assertTrue(n3 - n1 == ZERO)
-        self.assertTrue(n4 - n1 > ZERO)
+        self.assertTrue(decimal1 - decimal2 == ZERO)
+        self.assertTrue(decimal3 - decimal1 == ZERO)
+        self.assertTrue(decimal4 - decimal1 > ZERO)
 
-        self.assertTrue(n1 * three * n2 == zero_point_twelve)
-        self.assertTrue(n1 * three * n3 == zero_point_twelve)
-        self.assertTrue(n1 * three * n4 > zero_point_twelve)
+        self.assertTrue(decimal1 * three * decimal2 == zero_point_twelve)
+        self.assertTrue(decimal1 * three * decimal3 == zero_point_twelve)
+        self.assertTrue(decimal1 * three * decimal4 > zero_point_twelve)
 
-        self.assertTrue(n2 / n1 == one)
-        self.assertTrue(n3 / n1 == one)
-        self.assertTrue(n4 / n1 > one)
+        self.assertTrue(decimal2 / decimal1 == one)
+        self.assertTrue(decimal3 / decimal1 == one)
+        self.assertTrue(decimal4 / decimal1 > one)
 
         self.assertTrue(three // two == one)
-        self.assertTrue((three // two) + n3 - n1 == one)
-        self.assertTrue((three // two) + n4 - n1 > one)
+        self.assertTrue((three // two) + decimal3 - decimal1 == one)
+        self.assertTrue((three // two) + decimal4 - decimal1 > one)
 
         self.assertTrue(pow(three, two) == nine)
-        self.assertTrue(pow(three, two) + n3 - n1 == nine)
-        self.assertTrue(pow(three, two) + n4 - n1 > nine)
+        self.assertTrue(pow(three, two) + decimal3 - decimal1 == nine)
+        self.assertTrue(pow(three, two) + decimal4 - decimal1 > nine)
 
         self.assertTrue(nine % two == one)
-        self.assertTrue(nine % two + n3 - n1 == one)
-        self.assertTrue(nine % two + n4 - n1 > one)
+        self.assertTrue(nine % two + decimal3 - decimal1 == one)
+        self.assertTrue(nine % two + decimal4 - decimal1 > one)
 
     def test_bad_rp2_decimal(self) -> None:
-        # pylint: disable=pointless-statement
-        # pylint: disable=misplaced-comparison-constant
+        # pylint: disable=C0122, W0104
         one: RP2Decimal = RP2Decimal("1")
 
         # Test comparison operators
