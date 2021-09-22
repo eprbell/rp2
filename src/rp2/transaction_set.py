68053ad8a068538744086d2eb498471c5cf6f2fc
eprbell <77937475+eprbell@users.noreply.github.com>
2021-09-22 15:36:31 -0700

    Check to_year in add_entry().

--- src/rp2/transaction_set.py
+++ src/rp2/transaction_set.py
@@ -46,6 +46,10 @@ class TransactionSet(AbstractEntrySet):
 
     def add_entry(self, entry: AbstractEntry) -> None:
         AbstractTransaction.type_check("entry", entry)
+
+        if entry.timestamp.year > self.to_year:
+            return
+
         super().add_entry(entry)
 
     def _validate_entry(self, entry: AbstractEntry) -> None:
