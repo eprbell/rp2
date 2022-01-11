d983b5151f78aff067060b2ff0d955b22c477c77
eprbell <77937475+eprbell@users.noreply.github.com>
2022-01-11 13:40:35 -0800

    Updated to reflect new API and code structure for accounting method plugins

--- src/rp2/plugin/accounting_method/fifo.py
+++ src/rp2/plugin/accounting_method/fifo.py
@@ -12,67 +12,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Iterable, Iterator, cast
+from typing import Iterator, Optional
 
-from rp2.abstract_accounting_method import AbstractAccountingMethod
+from rp2.abstract_accounting_method import (
+    AbstractAccountingMethod,
+    FromLotsExhaustedException,
+    TaxableEventAndFromLot,
+    TaxableEventsExhaustedException,
+)
 from rp2.abstract_transaction import AbstractTransaction
-from rp2.configuration import MAX_YEAR, Configuration
-from rp2.gain_loss import GainLoss
-from rp2.gain_loss_set import GainLossSet
 from rp2.in_transaction import InTransaction
 from rp2.rp2_decimal import RP2Decimal
-from rp2.rp2_error import RP2ValueError
-from rp2.transaction_set import TransactionSet
 
 
 # FIFO accounting method
 class AccountingMethod(AbstractAccountingMethod):
-    def map_in_to_out_lots(
-        self, configuration: Configuration, asset: str, from_lot_set: TransactionSet, unfiltered_taxable_event_set: TransactionSet
-    ) -> GainLossSet:
 
-        gain_loss_set: GainLossSet = GainLossSet(configuration, asset, 0, MAX_YEAR)
+    __taxable_event_iterator: Iterator[AbstractTransaction]
+    __from_lot_iterator: Iterator[InTransaction]
 
-        taxable_event_iterator: Iterator[AbstractTransaction] = iter(cast(Iterable[AbstractTransaction], unfiltered_taxable_event_set))
-        from_lot_iterator: Iterator[InTransaction] = iter(cast(Iterable[InTransaction], from_lot_set))
+    # Iterators yield transactions in ascending chronological order
+    def initialize(self, taxable_event_iterator: Iterator[AbstractTransaction], from_lot_iterator: Iterator[InTransaction]) -> None:
+        self.__taxable_event_iterator = taxable_event_iterator
+        self.__from_lot_iterator = from_lot_iterator
 
+    def get_next_taxable_event_and_amount(
+        self, taxable_event: Optional[AbstractTransaction], from_lot: Optional[InTransaction], taxable_event_amount: RP2Decimal, from_lot_amount: RP2Decimal
+    ) -> TaxableEventAndFromLot:
         try:
-            gain_loss: GainLoss
-            taxable_event: AbstractTransaction = next(taxable_event_iterator)
-            taxable_event_amount: RP2Decimal = taxable_event.crypto_taxable_amount
-            from_lot: InTransaction = next(from_lot_iterator)
-            from_lot_amount: RP2Decimal = from_lot.crypto_in
+            new_taxable_event: AbstractTransaction = next(self.__taxable_event_iterator)
+        except StopIteration:
+            raise TaxableEventsExhaustedException() from None
+        return TaxableEventAndFromLot(
+            taxable_event=new_taxable_event,
+            from_lot=from_lot,
+            taxable_event_amount=new_taxable_event.crypto_taxable_amount,
+            from_lot_amount=from_lot_amount - taxable_event_amount,
+        )
 
-            while True:
-                if taxable_event.transaction_type.is_earn_type():
-                    # Handle earn-typed transactions first: they have no from-lot
-                    gain_loss = GainLoss(configuration, taxable_event_amount, taxable_event, None)
-                    gain_loss_set.add_entry(gain_loss)
-                    taxable_event = next(taxable_event_iterator)
-                    taxable_event_amount = taxable_event.crypto_taxable_amount
-                    continue
+    def get_from_lot_for_taxable_event(
+        self, taxable_event: AbstractTransaction, from_lot: Optional[InTransaction], taxable_event_amount: RP2Decimal, from_lot_amount: RP2Decimal
+    ) -> TaxableEventAndFromLot:
+        try:
+            new_from_lot: InTransaction = next(self.__from_lot_iterator)
+        except StopIteration:
+            raise FromLotsExhaustedException() from None
+        return TaxableEventAndFromLot(
+            taxable_event=taxable_event,
+            from_lot=new_from_lot,
+            taxable_event_amount=taxable_event_amount - from_lot_amount,
+            from_lot_amount=new_from_lot.crypto_in,
+        )
 
-                if taxable_event_amount == from_lot_amount:
-                    gain_loss = GainLoss(configuration, taxable_event_amount, taxable_event, from_lot)
-                    gain_loss_set.add_entry(gain_loss)
-                    taxable_event = next(taxable_event_iterator)
-                    taxable_event_amount = taxable_event.crypto_taxable_amount
-                    from_lot = next(from_lot_iterator)
-                    from_lot_amount = from_lot.crypto_in
-                elif taxable_event_amount < from_lot_amount:
-                    gain_loss = GainLoss(configuration, taxable_event_amount, taxable_event, from_lot)
-                    gain_loss_set.add_entry(gain_loss)
-                    from_lot_amount = from_lot_amount - taxable_event_amount
-                    taxable_event = next(taxable_event_iterator)
-                    taxable_event_amount = taxable_event.crypto_taxable_amount
-                else:  # taxable_amount > from_lot_amount
-                    gain_loss = GainLoss(configuration, from_lot_amount, taxable_event, from_lot)
-                    gain_loss_set.add_entry(gain_loss)
-                    taxable_event_amount = taxable_event_amount - from_lot_amount
-                    from_lot = next(from_lot_iterator)
-                    from_lot_amount = from_lot.crypto_in
-        except StopIteration as exception:
-            if cast(Iterator[InTransaction], exception.value) == from_lot_iterator:
-                raise RP2ValueError("Total in-transaction value < total taxable entries") from None
-
-        return gain_loss_set
+    def validate_from_lot_ancestor_timestamp(self, from_lot: InTransaction, from_lot_parent: InTransaction) -> bool:
+        return from_lot.timestamp >= from_lot_parent.timestamp
