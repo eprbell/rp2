813f534167515aa00f504ac5b9344da1bb07bb23
eprbell <77937475+eprbell@users.noreply.github.com>
2022-03-16 13:01:16 -0700

    Small refactoring to improve robustness

--- tests/ods_diff.py
+++ tests/ods_diff.py
@@ -46,19 +46,23 @@ def _row_as_string(row: Any) -> str:
 
 def _parse_cell_value(cell: Any) -> Any:
     value: Any
+    if cell.formula:
+        value = cell.formula
+        if _PURGE_HYPERLIKS and value.startswith("=HYPERLINK"):
+            value = value.split(";")[1].lstrip(' "').rstrip('")')
+    elif cell.value or cell.value == 0:
+        value = cell.value
+    else:
+        value = ""
     try:
-        if cell.formula:
-            value = cell.formula
-            if _PURGE_HYPERLIKS and value.startswith("=HYPERLINK"):
-                value = value.split(";")[1].lstrip(' "').rstrip('")')
-        elif cell.value or cell.value == 0:
-            value = round(float(cell.value), CRYPTO_DECIMALS)
-            if value == -0.0:
-                value = 0
-        else:
-            value = ""
+        value = round(float(value), CRYPTO_DECIMALS)
+        if value == -0.0:
+            value = 0
     except ValueError:
         value = cell.value
+
+    if value is None:
+        value = ""
 
     return value
 
