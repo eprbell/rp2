3c13344c5c5be5f3c02708d2c0177a8e22866c71
eprbell <77937475+eprbell@users.noreply.github.com>
2022-03-21 17:00:56 -0700

    Added unit tests for InTransaction crypto_fee

--- tests/test_ods_output_diff.py
+++ tests/test_ods_output_diff.py
@@ -38,6 +38,7 @@ class TestODSOutputDiff(AbstractTestODSOutputDiff):  # pylint: disable=too-many-
             AbstractTestODSOutputDiff._generate(cls.output_dir, test_name="test_data", config="test_data", method=method)
             AbstractTestODSOutputDiff._generate(cls.output_dir, test_name="test_data2", config="test_data", method=method)
             AbstractTestODSOutputDiff._generate(cls.output_dir, test_name="test_data3", config="test_data", method=method)
+            AbstractTestODSOutputDiff._generate(cls.output_dir, test_name="test_data4", config="test_data4", method=method)
             AbstractTestODSOutputDiff._generate(cls.output_dir, test_name="test_many_year_data", config="test_data", method=method)
             AbstractTestODSOutputDiff._generate(
                 cls.output_dir, test_name="test_data3", config="test_data", method=method, from_date=date(2019, 12, 1), to_date=date(2020, 4, 1)
@@ -99,6 +100,14 @@ class TestODSOutputDiff(AbstractTestODSOutputDiff):  # pylint: disable=too-many-
     def test_test_data3_tax_report_us(self) -> None:
         for method in self.METHODS:
             self._compare(output_dir=self.output_dir, test_name="test_data3", method=method, output_plugin=OutputPlugins.TAX_REPORT_US)
+
+    def test_test_data4_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_data4", method=method, output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_data4_tax_report_us(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_data4", method=method, output_plugin=OutputPlugins.TAX_REPORT_US)
 
     def test_test_many_year_data_rp2_full_report(self) -> None:
         for method in self.METHODS:
