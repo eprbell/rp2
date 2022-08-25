796b1d896f46899e2e02f68e7b1e3d0e5a94a846
eprbell <77937475+eprbell@users.noreply.github.com>
2022-08-24 19:19:27 -0700

    Fixed copy-paste error in copyright line

--- src/rp2/plugin/accounting_method/lifo.py
+++ src/rp2/plugin/accounting_method/lifo.py
@@ -1,4 +1,4 @@
-# Copyright 2022 ninideol
+# Copyright 2021 eprbell
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
