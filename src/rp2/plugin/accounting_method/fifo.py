8d3164e697e6064038eca7c904b73b08585c19b3
eprbell <77937475+eprbell@users.noreply.github.com>
2022-08-29 10:31:14 -0700

    Made API fully encapsulated

--- src/rp2/plugin/accounting_method/fifo.py
+++ src/rp2/plugin/accounting_method/fifo.py
@@ -12,29 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Optional
+from typing import Optional
 
-from rp2.abstract_specific_id import AbstractSpecificId, AcquiredLotAndAmount
+from rp2.abstract_accounting_method import AbstractAccountingMethod
+from rp2.abstract_accounting_method import AcquiredLotCandidates, AcquiredLotCandidatesOrder, AcquiredLotAndAmount
+from rp2.abstract_transaction import AbstractTransaction
 from rp2.in_transaction import InTransaction
 from rp2.rp2_decimal import ZERO, RP2Decimal
 
 
 # FIFO plugin. See https://www.investopedia.com/terms/l/fifo.asp.
-class AccountingMethod(AbstractSpecificId):
-
-    def _seek_non_exhausted_acquired_lot_before_index(self, acquired_lot_list: List[InTransaction], last_valid_index: int) -> Optional[AcquiredLotAndAmount]:
+class AccountingMethod(AbstractAccountingMethod):
+    def seek_non_exhausted_acquired_lot(
+        self,
+        lot_candidates: AcquiredLotCandidates,
+        taxable_event: Optional[AbstractTransaction],
+        taxable_event_amount: RP2Decimal,
+    ) -> Optional[AcquiredLotAndAmount]:
         # This loop causes O(m*n) complexity, where m is the number of acquired lots and n in the number of taxable events. The taxable
         # event loop is in the caller. Non-trivial optimizations are possible using different data structures but they need to be researched.
         selected_acquired_lot_amount: RP2Decimal = ZERO
         selected_acquired_lot: Optional[InTransaction] = None
-        for index in range(0, last_valid_index + 1):
+        acquired_lot: InTransaction
+        for acquired_lot in lot_candidates:
             acquired_lot_amount: RP2Decimal = ZERO
-            acquired_lot: InTransaction = acquired_lot_list[index]
 
-            if not self._has_partial_amount(acquired_lot):
+            if not lot_candidates.has_partial_amount(acquired_lot):
                 acquired_lot_amount = acquired_lot.crypto_in
-            elif self._get_partial_amount(acquired_lot) > ZERO:
-                acquired_lot_amount = self._get_partial_amount(acquired_lot)
+            elif lot_candidates.get_partial_amount(acquired_lot) > ZERO:
+                acquired_lot_amount = lot_candidates.get_partial_amount(acquired_lot)
             else:
                 # The acquired lot has zero partial amount
                 continue
@@ -44,9 +50,9 @@ class AccountingMethod(AbstractSpecificId):
             break
 
         if selected_acquired_lot_amount > ZERO and selected_acquired_lot:
-            self._clear_partial_amount(selected_acquired_lot)
+            lot_candidates.clear_partial_amount(selected_acquired_lot)
             return AcquiredLotAndAmount(acquired_lot=selected_acquired_lot, amount=selected_acquired_lot_amount)
         return None
 
-    def validate_acquired_lot_ancestor_timestamp(self, acquired_lot: InTransaction, acquired_lot_parent: InTransaction) -> bool:
-        return acquired_lot.timestamp >= acquired_lot_parent.timestamp
+    def lot_candidates_order(self) -> AcquiredLotCandidatesOrder:
+        return AcquiredLotCandidatesOrder.OLDER_TO_NEWER
--- src/rp2/plugin/accounting_method/lifo.py
+++ src/rp2/plugin/accounting_method/lifo.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Optional
+from typing import Optional
 
-from rp2.abstract_specific_id import AbstractSpecificId, AcquiredLotAndAmount
+from rp2.abstract_accounting_method import AbstractAccountingMethod
+from rp2.abstract_accounting_method import AcquiredLotCandidates, AcquiredLotCandidatesOrder, AcquiredLotAndAmount
+from rp2.abstract_transaction import AbstractTransaction
 from rp2.in_transaction import InTransaction
 from rp2.rp2_decimal import ZERO, RP2Decimal
 
@@ -22,21 +24,25 @@ from rp2.rp2_decimal import ZERO, RP2Decimal
 # LIFO plugin. See https://www.investopedia.com/terms/l/lifo.asp. Note that under LIFO the date acquired must still be before or on the date sold:
 # see this discussion for details,
 # https://ttlc.intuit.com/community/investments-and-rental-properties/discussion/using-lifo-method-for-cryptocurrency-or-even-stock-cost-basis/00/1433542
-class AccountingMethod(AbstractSpecificId):
-
-    def _seek_non_exhausted_acquired_lot_before_index(self, acquired_lot_list: List[InTransaction], last_valid_index: int) -> Optional[AcquiredLotAndAmount]:
+class AccountingMethod(AbstractAccountingMethod):
+    def seek_non_exhausted_acquired_lot(
+        self,
+        lot_candidates: AcquiredLotCandidates,
+        taxable_event: Optional[AbstractTransaction],
+        taxable_event_amount: RP2Decimal,
+    ) -> Optional[AcquiredLotAndAmount]:
         # This loop causes O(m*n) complexity, where m is the number of acquired lots and n in the number of taxable events. The taxable
         # event loop is in the caller. Non-trivial optimizations are possible using different data structures but they need to be researched.
         selected_acquired_lot_amount: RP2Decimal = ZERO
         selected_acquired_lot: Optional[InTransaction] = None
-        for index in range(last_valid_index, -1, -1):
+        acquired_lot: InTransaction
+        for acquired_lot in lot_candidates:
             acquired_lot_amount: RP2Decimal = ZERO
-            acquired_lot: InTransaction = acquired_lot_list[index]
 
-            if not self._has_partial_amount(acquired_lot):
+            if not lot_candidates.has_partial_amount(acquired_lot):
                 acquired_lot_amount = acquired_lot.crypto_in
-            elif self._get_partial_amount(acquired_lot) > ZERO:
-                acquired_lot_amount = self._get_partial_amount(acquired_lot)
+            elif lot_candidates.get_partial_amount(acquired_lot) > ZERO:
+                acquired_lot_amount = lot_candidates.get_partial_amount(acquired_lot)
             else:
                 # The acquired lot has zero partial amount
                 continue
@@ -46,10 +52,9 @@ class AccountingMethod(AbstractSpecificId):
             break
 
         if selected_acquired_lot_amount > ZERO and selected_acquired_lot:
-            self._clear_partial_amount(selected_acquired_lot)
+            lot_candidates.clear_partial_amount(selected_acquired_lot)
             return AcquiredLotAndAmount(acquired_lot=selected_acquired_lot, amount=selected_acquired_lot_amount)
         return None
 
-    def validate_acquired_lot_ancestor_timestamp(self, acquired_lot: InTransaction, acquired_lot_parent: InTransaction) -> bool:
-        # In LIFO the acquired_lot chain can have non-monotonic timestamps, so no validation is possible. Returning True means the validation never fails.
-        return True
+    def lot_candidates_order(self) -> AcquiredLotCandidatesOrder:
+        return AcquiredLotCandidatesOrder.NEWER_TO_OLDER
