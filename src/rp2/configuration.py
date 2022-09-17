eecaf3e3582336487ff8c4dbc74c04354f522bb2
eprbell <77937475+eprbell@users.noreply.github.com>
2022-09-17 11:37:44 +0200

    Updated to parse INI configurations instead of JSON

--- src/rp2/configuration.py
+++ src/rp2/configuration.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from configparser import ConfigParser, SectionProxy
+from enum import Enum
 import json
 from datetime import date, datetime
 from pathlib import Path
@@ -31,6 +33,87 @@ MAX_DATE: date = date(9999, 12, 31)
 REPORT_GENERATOR_PACKAGE = "rp2.plugin.report"
 
 
+class Keyword(Enum):
+    ACCOUNTING_METHODS: str = "accounting_methods"
+    ASSET: str = "asset"
+    ASSETS: str = "assets"
+    CRYPTO_FEE: str = "crypto_fee"
+    CRYPTO_IN: str = "crypto_in"
+    CRYPTO_OUT_NO_FEE: str = "crypto_out_no_fee"
+    CRYPTO_OUT_WITH_FEE: str = "crypto_out_with_fee"
+    CRYPTO_RECEIVED: str = "crypto_received"
+    CRYPTO_SENT: str = "crypto_sent"
+    EXCHANGE: str = "exchange"
+    EXCHANGES: str = "exchanges"
+    FIAT_FEE: str = "fiat_fee"
+    FIAT_IN_NO_FEE: str = "fiat_in_no_fee"
+    FIAT_IN_WITH_FEE: str = "fiat_in_with_fee"
+    FIAT_OUT_NO_FEE: str = "fiat_out_no_fee"
+    FROM_EXCHANGE: str = "from_exchange"
+    FROM_HOLDER: str = "from_holder"
+    GENERAL: str = "general"
+    GENERATORS: str = "generators"
+    HOLDER: str = "holder"
+    HOLDERS: str = "holders"
+    IN_HEADER: str = "in_header"
+    INTRA_HEADER: str = "intra_header"
+    NOTES: str = "notes"
+    OUT_HEADER: str = "out_header"
+    SPOT_PRICE: str = "spot_price"
+    TIMESTAMP: str = "timestamp"
+    TO_EXCHANGE: str = "to_exchange"
+    TO_HOLDER: str = "to_holder"
+    TRANSACTION_TYPE: str = "transaction_type"
+    UNIQUE_ID: str = "unique_id"
+
+
+_HEADER_COLUMNS: Dict[str, Set[str]] = {
+    Keyword.IN_HEADER.value: {
+        Keyword.TIMESTAMP.value,
+        Keyword.ASSET.value,
+        Keyword.EXCHANGE.value,
+        Keyword.HOLDER.value,
+        Keyword.TRANSACTION_TYPE.value,
+        Keyword.SPOT_PRICE.value,
+        Keyword.CRYPTO_IN.value,
+        Keyword.CRYPTO_FEE.value,
+        Keyword.FIAT_IN_NO_FEE.value,
+        Keyword.FIAT_IN_WITH_FEE.value,
+        Keyword.FIAT_FEE.value,
+        Keyword.UNIQUE_ID.value,
+        Keyword.NOTES.value,
+    },
+    Keyword.OUT_HEADER.value: {
+        Keyword.TIMESTAMP.value,
+        Keyword.ASSET.value,
+        Keyword.EXCHANGE.value,
+        Keyword.HOLDER.value,
+        Keyword.TRANSACTION_TYPE.value,
+        Keyword.SPOT_PRICE.value,
+        Keyword.CRYPTO_OUT_NO_FEE.value,
+        Keyword.CRYPTO_FEE.value,
+        Keyword.CRYPTO_OUT_WITH_FEE.value,
+        Keyword.FIAT_OUT_NO_FEE.value,
+        Keyword.FIAT_FEE.value,
+        Keyword.UNIQUE_ID.value,
+        Keyword.NOTES.value,
+    },
+    Keyword.INTRA_HEADER.value: {
+        Keyword.TIMESTAMP.value,
+        Keyword.ASSET.value,
+        Keyword.FROM_EXCHANGE.value,
+        Keyword.FROM_HOLDER.value,
+        Keyword.TO_EXCHANGE.value,
+        Keyword.TO_HOLDER.value,
+        Keyword.SPOT_PRICE.value,
+        Keyword.CRYPTO_SENT.value,
+        Keyword.CRYPTO_RECEIVED.value,
+        Keyword.UNIQUE_ID.value,
+        Keyword.NOTES.value,
+    },
+}
+
+
 class Configuration:  # pylint: disable=too-many-public-methods
     @classmethod
     def type_check(cls, name: str, instance: "Configuration") -> "Configuration":
@@ -39,7 +122,7 @@ class Configuration:  # pylint: disable=too-many-public-methods
             raise RP2TypeError(f"Parameter '{name}' is not of type {cls.__name__}: {instance}")
         return instance
 
-    def __init__(
+    def __init__(  # pylint: disable=too-many-branches
         self,
         configuration_path: str,
         country: AbstractCountry,
@@ -58,48 +141,149 @@ class Configuration:  # pylint: disable=too-many-public-methods
         if self.__from_date > self.__to_date:
             raise RP2ValueError("Parameter from_date cannot be greater than to_date")
 
-        self.__in_header: Dict[str, int]
-        self.__out_header: Dict[str, int]
-        self.__intra_header: Dict[str, int]
-        self.__assets: Set[str]
-        self.__exchanges: Set[str]
-        self.__holders: Set[str]
-        self.__generators: Set[str]
-        self.__years_2_accounting_method_names: Dict[int, str]
+        self.__in_header: Dict[str, int] = {}
+        self.__out_header: Dict[str, int] = {}
+        self.__intra_header: Dict[str, int] = {}
+        self.__assets: Set[str] = set()
+        self.__exchanges: Set[str] = set()
+        self.__holders: Set[str] = set()
+        self.__generators: Set[str] = {f"{REPORT_GENERATOR_PACKAGE}.{generator}" for generator in country.get_report_generators()}
+        self.__years_2_accounting_method_names: Dict[int, str] = {}
 
         if not Path(configuration_path).exists():
             raise RP2ValueError(f"Error: {configuration_path} does not exist")
 
         with open(configuration_path, encoding="utf-8") as configuration_file:
-            # This json_configuration is validated by jsonschema, so we can disable static type checking for it:
-            # it adds complexity but not much value over jsonschema checks
-            json_configuration: Any = json.load(configuration_file)
-            validate(instance=json_configuration, schema=CONFIGURATION_SCHEMA)
+            try:
+                # JSON configuration is deprecated: if JSON is detected, raise an exception
+                json_configuration: Any = json.load(configuration_file)
+                validate(instance=json_configuration, schema=CONFIGURATION_SCHEMA)
+                raise RP2ValueError(
+                    "Configuration file uses the deprecated JSON-format, instead of the INI format. "
+                    f"To convert the JSON configuration to INI use: rp2_config {configuration_path}"
+                )
+            except json.JSONDecodeError:
+                pass
 
-            self.__in_header = json_configuration["in_header"]
-            self.__out_header = json_configuration["out_header"]
-            self.__intra_header = json_configuration["intra_header"]
-            self.__assets = set(json_configuration["assets"])
-            self.__exchanges = set(json_configuration["exchanges"])
-            self.__holders = set(json_configuration["holders"])
-            self.__generators = {f"{REPORT_GENERATOR_PACKAGE}.{generator}" for generator in country.get_report_generators()}
-            if "generators" in json_configuration:
-                self.__generators = set(json_configuration["generators"])
-            self.__years_2_accounting_method_names = {}
-            if "accounting_methods" in json_configuration:
-                year_string_2_accounting_method_names = dict(json_configuration["accounting_methods"])
-                for year_string, accounting_method_name in year_string_2_accounting_method_names.items():
-                    try:
-                        self.__years_2_accounting_method_names[int(year_string)] = accounting_method_name
-                    except ValueError as exc:
-                        raise RP2ValueError(f"Invalid year value in accounting method section of configuration file: {year_string}") from exc
-                    except TypeError as exc:
-                        raise RP2TypeError(f"Invalid year type in accounting method section of configuration file: {year_string}") from exc
+            ini_configuration: ConfigParser = ConfigParser()
+            ini_configuration.read(configuration_path)
+
+            for section_name in ini_configuration.sections():
+                section_name = section_name.strip()
+                normalized_section_name: str = section_name.split(" ", 1)[0].strip()
+                if normalized_section_name == Keyword.GENERAL.value:
+                    if self.__assets or self.__exchanges or self.__holders:
+                        raise RP2ValueError(f"{configuration_path}: section '{normalized_section_name}' found multiple times in configuration file")
+                    self.__assets = self._validate_string_set(Keyword.ASSETS.value, ini_configuration[section_name], configuration_path)
+                    self.__exchanges = self._validate_string_set(Keyword.EXCHANGES.value, ini_configuration[section_name], configuration_path)
+                    self.__holders = self._validate_string_set(Keyword.HOLDERS.value, ini_configuration[section_name], configuration_path)
+                    if Keyword.GENERATORS.value in ini_configuration:
+                        self.__generators = self._validate_string_set(Keyword.GENERATORS.value, ini_configuration[section_name], configuration_path)
+                elif normalized_section_name == Keyword.IN_HEADER.value:
+                    if self.__in_header:
+                        raise RP2ValueError(f"{configuration_path}: section '{normalized_section_name}' found multiple times in configuration file")
+                    self.__in_header = self._validate_header_section(ini_configuration[section_name], normalized_section_name, configuration_path)
+                elif normalized_section_name == Keyword.OUT_HEADER.value:
+                    if self.__out_header:
+                        raise RP2ValueError(f"{configuration_path}: section '{normalized_section_name}' found multiple times in configuration file")
+                    self.__out_header = self._validate_header_section(ini_configuration[section_name], normalized_section_name, configuration_path)
+                elif normalized_section_name == Keyword.INTRA_HEADER.value:
+                    if self.__intra_header:
+                        raise RP2ValueError(f"{configuration_path}: section '{normalized_section_name}' found multiple times in configuration file")
+                    self.__intra_header = self._validate_header_section(ini_configuration[section_name], normalized_section_name, configuration_path)
+                elif normalized_section_name == Keyword.ACCOUNTING_METHODS.value:
+                    if self.__years_2_accounting_method_names:
+                        raise RP2ValueError(f"{configuration_path}: section '{normalized_section_name}' found multiple times in configuration file")
+                    self.__years_2_accounting_method_names = self._validate_accounting_method_section(ini_configuration[section_name], configuration_path)
+                else:
+                    raise RP2ValueError(f"{configuration_path}: invalid section '{section_name}' found")
+
+        if not self.__assets:
+            raise RP2ValueError(f"{configuration_path}: no '{Keyword.ASSETS.value}' field defined in {Keyword.GENERAL.value} section")
+        if not self.__exchanges:
+            raise RP2ValueError(f"{configuration_path}: no '{Keyword.EXCHANGES.value}' field defined in {Keyword.GENERAL.value} section")
+        if not self.__holders:
+            raise RP2ValueError(f"{configuration_path}: no '{Keyword.HOLDERS.value}' field defined in {Keyword.GENERAL.value} section")
+        if not self.__in_header:
+            raise RP2ValueError(f"{configuration_path}: empty '{Keyword.IN_HEADER.value}' section")
+        if not self.__out_header:
+            raise RP2ValueError(f"{configuration_path}: empty '{Keyword.OUT_HEADER.value}' section")
+        if not self.__intra_header:
+            raise RP2ValueError(f"{configuration_path}: empty '{Keyword.INTRA_HEADER.value}' section")
 
         # Used by __repr__()
         self.__sorted_assets: List[str] = sorted(self.__assets)
         self.__sorted_exchanges: List[str] = sorted(self.__exchanges)
         self.__sorted_holders: List[str] = sorted(self.__holders)
+
+    def _validate_string_set(self, field_name: str, section: SectionProxy, configuration_path: str) -> Set[str]:
+        if field_name not in section:
+            raise RP2ValueError(f"{configuration_path}: section '{section.name}' doesn't contain mandatory field '{field_name}'")
+        list_as_string = section[field_name]
+        if not list_as_string or not list_as_string.strip():
+            raise RP2ValueError(f"{configuration_path}: field '{field_name}' in section '{section.name}' cannot be empty")
+        list_as_values: List[str] = [value.strip() for value in list_as_string.split(",")]
+        if not list_as_values:
+            raise RP2ValueError(f"{configuration_path}: field '{field_name}' in section '{section.name}' cannot be empty")
+        result: Set[str] = set()
+        for element in list_as_values:
+            if not element:
+                raise RP2ValueError(f"{configuration_path}: field '{field_name}' in section '{section.name}' cannot contain empty elements")
+            if not isinstance(element, str):
+                raise RP2ValueError(f"{configuration_path}: field '{field_name}' in section '{section.name}' cannot contain non-string elements")
+            if element in result:
+                raise RP2ValueError(f"{configuration_path}: field '{field_name}' in section '{section.name}' contains duplicate elements: {element}")
+            result.add(element)
+        return set(list_as_values)
+
+    def _validate_header_section(self, section: SectionProxy, normalized_section_name: str, configuration_path: str) -> Dict[str, int]:
+        if not section:
+            raise RP2ValueError(f"{configuration_path}: section '{section.name}' cannot be empty")
+        header_2_column: Dict[str, int] = {}
+        column_to_header: Dict[int, str] = {}
+        for header, column in section.items():
+            try:
+                column_value: int = int(column.strip())
+                if column_value < 0:
+                    raise RP2ValueError(
+                        f"{configuration_path}: invalid column value for field '{header}' in section '{section.name}' (positive integer was expected): {column}"
+                    )
+                if column_value in column_to_header:
+                    raise RP2ValueError(
+                        f"{configuration_path}: fields '{column_to_header[column_value]}' and "
+                        f"'{header}' have the same value in section '{section.name}': {column_value}"
+                    )
+                if header not in _HEADER_COLUMNS[normalized_section_name]:
+                    raise RP2ValueError(f"{configuration_path}: invalid column header in section '{section.name}': {header}")
+                header_2_column[header.strip()] = column_value
+                column_to_header[column_value] = header
+            except ValueError as exc:
+                raise RP2ValueError(
+                    f"{configuration_path}: invalid column value for field '{header}' in section '{section.name}' (integer was expected): {column}"
+                ) from exc
+            except TypeError as exc:
+                raise RP2ValueError(
+                    f"{configuration_path}: invalid value type for field '{header}' in section '{section.name}' (integer was expected): {column}"
+                ) from exc
+        return header_2_column
+
+    def _validate_accounting_method_section(self, section: SectionProxy, configuration_path: str) -> Dict[int, str]:
+        if not section:
+            raise RP2ValueError(f"{configuration_path}: section '{section.name}' cannot be empty")
+        result: Dict[int, str] = {}
+        for year, method in section.items():
+            try:
+                numeric_year: int = int(year.strip())
+                if numeric_year < MIN_DATE.year:
+                    raise RP2ValueError(
+                        f"{configuration_path}: invalid year value in accounting method section (integer > {MIN_DATE.year} was expected): {year}"
+                    )
+                result[numeric_year] = method.strip()
+            except ValueError as exc:
+                raise RP2ValueError(f"{configuration_path}: invalid year value in accounting method section (integer was expected): {year}") from exc
+            except TypeError as exc:
+                raise RP2ValueError(f"{configuration_path}: invalid year type in accounting method section (integer was expected): {year}") from exc
+        return result
 
     def __repr__(self) -> str:
         return (
