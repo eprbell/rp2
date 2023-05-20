92f4ae1ffba8a2bf1871565932228cd5d5d33dd1
Larision <fjavigar@hotmail.com>
2023-05-20 19:08:25 +0200

    copyrights

--- src/rp2/plugin/country/es.py
+++ src/rp2/plugin/country/es.py
@@ -1,4 +1,4 @@
-# Copyright 2021 eprbell
+# Copyright 2023 Larision
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
