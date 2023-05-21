c29442e413769567102fe1bda40ed34c49c7a14e
Larision <fjavigar@hotmail.com>
2023-05-22 00:40:40 +0200

    copyright update

--- tests/test_ods_output_diff_es.py
+++ tests/test_ods_output_diff_es.py
@@ -1,5 +1,4 @@
-# Copyright 2022 Neal Chambers
-#
+# Copyright 2023 Larision
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
