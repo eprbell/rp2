66c3e87a0bb3d943e6794176f979b0540f9cd6c8
eprbell <77937475+eprbell@users.noreply.github.com>
2024-06-24 22:19:33 -0700

    Black formatting

--- src/rp2/plugin/country/ie.py
+++ src/rp2/plugin/country/ie.py
@@ -19,6 +19,7 @@ from rp2.abstract_country import AbstractCountry
 from rp2.rp2_main import rp2_main
 import sys
 
+
 # Ireland-specific class
 class IE(AbstractCountry):
     def __init__(self) -> None:
@@ -38,11 +39,7 @@ class IE(AbstractCountry):
 
     # Default set of generators to use if the user doesn't specify them on the command line
     def get_report_generators(self) -> Set[str]:
-        return {
-            "open_positions",
-            "rp2_full_report",
-            "ie.tax_report_ie"
-        }
+        return {"open_positions", "rp2_full_report", "ie.tax_report_ie"}
 
     # Default language to use at report generation if the user doesn't specify it on the command line (in ISO 639-1 format)
     def get_default_generation_language(self) -> str:
