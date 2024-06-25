0428920ba19209e0f6c2fd613dc953071c4d0717
eprbell <77937475+eprbell@users.noreply.github.com>
2024-06-24 22:22:42 -0700

    Added generic country support

---
+++ src/rp2/plugin/country/generic.py
@@ -0,0 +1,71 @@
+# Copyright 2021 eprbell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from typing import Set
+
+from rp2.abstract_country import AbstractCountry
+from rp2.rp2_error import RP2ValueError
+from rp2.rp2_main import rp2_main
+
+
+# Generic country class
+class Generic(AbstractCountry):
+    def __init__(self) -> None:
+        currency_iso_code = os.environ.get("CURRENCY_CODE")
+        if not currency_iso_code:
+            raise RP2ValueError("CURRENCY_CODE environment variable not found: it is required to use the generic country plugin")
+        long_term_capital_gain_period = os.environ.get("LONG_TERM_CAPITAL_GAINS")
+        if not long_term_capital_gain_period:
+            raise RP2ValueError("LONG_TERM_CAPITAL_GAINS environment variable not found: it is required to use the generic country plugin")
+        try:
+            self.__long_term_capital_gain_period = int(long_term_capital_gain_period)
+        except (ValueError, TypeError) as exc:
+            raise RP2ValueError(
+                f"LONG_TERM_CAPITAL_GAINS environment variable has value " f"'{long_term_capital_gain_period}', which is not convertible to integer"
+            ) from exc
+        if self.__long_term_capital_gain_period < 0:
+            raise RP2ValueError(
+                f"LONG_TERM_CAPITAL_GAINS environment variable has negative value " f"'{long_term_capital_gain_period}': it should be >= 0 instead"
+            )
+        super().__init__("generic", currency_iso_code)
+
+    # Measured in days
+    def get_long_term_capital_gain_period(self) -> int:
+        return self.__long_term_capital_gain_period
+
+    # Default accounting method to use if the user doesn't specify one on the command line
+    def get_default_accounting_method(self) -> str:
+        return "fifo"
+
+    # Set of accounting methods accepted in the country
+    def get_accounting_methods(self) -> Set[str]:
+        return {"fifo", "lifo", "hifo"}
+
+    # Default set of generators to use if the user doesn't specify them on the command line
+    def get_report_generators(self) -> Set[str]:
+        return {
+            "open_positions",
+            "rp2_full_report",
+        }
+
+    # Default language to use at report generation if the user doesn't specify it on the command line (in ISO 639-1 format)
+    def get_default_generation_language(self) -> str:
+        return "en"
+
+
+# US-specific entry point
+def rp2_entry() -> None:
+    rp2_main(Generic())
---
+++ tests/test_ods_output_diff_generic.py
@@ -0,0 +1,413 @@
+# Copyright 2021 eprbell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import unittest
+from datetime import date
+from pathlib import Path
+
+from abstract_test_ods_output_diff import AbstractTestODSOutputDiff, OutputPlugins
+
+ROOT_PATH: Path = Path(os.path.dirname(__file__)).parent.absolute()
+
+
+class TestODSOutputDiff(AbstractTestODSOutputDiff):  # pylint: disable=too-many-public-methods
+    output_dir: Path
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.output_dir = ROOT_PATH / Path("output") / Path(cls.__module__)
+
+        shutil.rmtree(cls.output_dir, ignore_errors=True)
+
+        env = {
+            "CURRENCY_CODE": "eur",
+            "LONG_TERM_CAPITAL_GAINS": "1000000000",
+        }
+
+        for method in AbstractTestODSOutputDiff.METHODS:
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir, test_name="crypto_example", config="crypto_example", method=method, country="generic", allow_negative_balances=True, env=env
+            )
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir, test_name="test_data", config="test_data", method=method, country="generic", allow_negative_balances=True, env=env
+            )
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir, test_name="test_data2", config="test_data", method=method, country="generic", allow_negative_balances=True, env=env
+            )
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir, test_name="test_data3", config="test_data", method=method, country="generic", allow_negative_balances=True, env=env
+            )
+            AbstractTestODSOutputDiff._generate(cls.output_dir, test_name="test_data4", config="test_data4", method=method, country="generic", env=env)
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir, test_name="test_hifo", config="test_data", method=method, country="generic", allow_negative_balances=True, env=env
+            )
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir, test_name="test_hifo2", config="test_data", method=method, country="generic", allow_negative_balances=True, env=env
+            )
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir, test_name="test_many_year_data", config="test_data", method=method, country="generic", allow_negative_balances=True, env=env
+            )
+            AbstractTestODSOutputDiff._generate(
+                cls.output_dir,
+                test_name="test_data3",
+                config="test_data",
+                method=method,
+                country="generic",
+                from_date=date(2019, 12, 1),
+                to_date=date(2020, 4, 1),
+                allow_negative_balances=True,
+                env=env,
+            )
+
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            to_date=date(2016, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            to_date=date(2017, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            to_date=date(2018, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            to_date=date(2019, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            to_date=date(2020, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2017, 1, 1),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2018, 1, 1),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2019, 1, 1),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2020, 1, 1),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2021, 1, 1),
+            allow_negative_balances=True,
+            env=env,
+        )
+
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2017, 1, 1),
+            to_date=date(2019, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2018, 1, 1),
+            to_date=date(2019, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_many_year_data",
+            config="test_data",
+            method="fifo",
+            country="generic",
+            from_date=date(2019, 1, 1),
+            to_date=date(2019, 12, 31),
+            allow_negative_balances=True,
+            env=env,
+        )
+        AbstractTestODSOutputDiff._generate(
+            cls.output_dir,
+            test_name="test_data_multi_method",
+            config="test_data_multi_method",
+            method="mixed",
+            country="generic",
+            allow_negative_balances=True,
+            env=env,
+        )
+
+    def setUp(self) -> None:
+        self.maxDiff = None  # pylint: disable=invalid-name
+
+    def test_crypto_example_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="crypto_example", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_data_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_data", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_data2_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_data2", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_data3_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_data3", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_data4_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_data4", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_hifo_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_hifo", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_hifo2_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(output_dir=self.output_dir, test_name="test_hifo2", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT)
+
+    def test_test_many_year_data_rp2_full_report(self) -> None:
+        for method in self.METHODS:
+            self._compare(
+                output_dir=self.output_dir, test_name="test_many_year_data", method=method, country="generic", output_plugin=OutputPlugins.RP2_FULL_REPORT
+            )
+
+    def test_test_data3_rp2_full_report_2019_12_01_2020_04_01(self) -> None:
+        for method in self.METHODS:
+            self._compare(
+                output_dir=self.output_dir,
+                test_name="test_data3",
+                method=method,
+                country="generic",
+                output_plugin=OutputPlugins.RP2_FULL_REPORT,
+                from_date=date(2019, 12, 1),
+                to_date=date(2020, 4, 1),
+            )
+
+    def test_test_many_year_data_rp2_full_report_0_2016(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            to_date=date(2016, 12, 31),
+        )
+
+    def test_test_many_year_data_rp2_full_report_0_2017(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            to_date=date(2017, 12, 31),
+        )
+
+    def test_test_many_year_data_rp2_full_report_0_2018(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            to_date=date(2018, 12, 31),
+        )
+
+    def test_test_many_year_data_rp2_full_report_0_2019(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            to_date=date(2019, 12, 31),
+        )
+
+    def test_test_many_year_data_rp2_full_report_0_2020(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            to_date=date(2020, 12, 31),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2017_infinity(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2017, 1, 1),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2018_infinity(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2018, 1, 1),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2019_infinity(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2019, 1, 1),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2020_infinity(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2020, 1, 1),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2021_infinity(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2021, 1, 1),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2017_2019(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2017, 1, 1),
+            to_date=date(2019, 12, 31),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2018_2019(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2018, 1, 1),
+            to_date=date(2019, 12, 31),
+        )
+
+    def test_test_many_year_data_rp2_full_report_2019_2019(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_many_year_data",
+            method="fifo",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+            from_date=date(2019, 1, 1),
+            to_date=date(2019, 12, 31),
+        )
+
+    def test_test_data_multi_method_rp2_full_report(self) -> None:
+        self._compare(
+            output_dir=self.output_dir,
+            test_name="test_data_multi_method",
+            method="mixed",
+            country="generic",
+            output_plugin=OutputPlugins.RP2_FULL_REPORT,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
