b22e4e0dc9e7dc07a3ce36ba2aa1ac790e59257a
eprbell <77937475+eprbell@users.noreply.github.com>
2024-12-21 17:21:51 -0800

    Initial version of transfer analysis

---
+++ src/rp2/per_wallet_tax_engine.py
@@ -0,0 +1,237 @@
+# Copyright 2024 eprbell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import date
+from typing import Dict, List, Optional
+
+from rp2.configuration import Configuration
+from rp2.abstract_accounting_method import AbstractAccountingMethod, AbstractAcquiredLotCandidates, AcquiredLotAndAmount
+from rp2.in_transaction import Account, InTransaction
+from rp2.input_data import InputData
+from rp2.intra_transaction import IntraTransaction
+from rp2.out_transaction import OutTransaction
+from rp2.rp2_decimal import ZERO, RP2Decimal
+from rp2.rp2_error import RP2ValueError
+from rp2.transaction_set import TransactionSet
+
+
+class _PerWalletTransactions:
+    def __init__(self, configuration: Configuration, asset: str, lot_selection_method: AbstractAccountingMethod, from_date: date, to_date: date):
+        self.__asset = asset
+        # Method to decide which lot to pick when transferring funds.
+        self.__lot_selection_method = lot_selection_method
+        acquired_lot_2_partial_amount: Dict[InTransaction, RP2Decimal] = {}
+        acquired_lot_list: List[InTransaction] = []
+        self.__in_transactions: AbstractAcquiredLotCandidates = lot_selection_method.create_lot_candidates(
+            acquired_lot_list=acquired_lot_list, acquired_lot_2_partial_amount=acquired_lot_2_partial_amount
+        )
+        self.__out_transactions: TransactionSet = TransactionSet(configuration, "OUT", asset, from_date, to_date)
+        self.__intra_transactions: TransactionSet = TransactionSet(configuration, "INTRA", asset, from_date, to_date)
+
+    @property
+    def asset(self) -> str:
+        return self.__asset
+
+    @property
+    def lot_selection_method(self) -> AbstractAccountingMethod:
+        return self.__lot_selection_method
+
+    @property
+    def in_transactions(self) -> AbstractAcquiredLotCandidates:
+        return self.__in_transactions
+
+    @property
+    def out_transactions(self) -> TransactionSet:
+        return self.__out_transactions
+
+    @property
+    def intra_transactions(self) -> TransactionSet:
+        return self.__intra_transactions
+
+
+# Utility function to create an artificial InTransaction modeling the "to" side of an IntraTransaction
+def _create_to_in_transaction(
+    configuration: Configuration, from_in_transaction: InTransaction, transfer_transaction: IntraTransaction, amount: RP2Decimal
+) -> InTransaction:
+    artificial_id = configuration.get_new_artificial_id()
+    result = InTransaction(
+        configuration=configuration,
+        timestamp=from_in_transaction.timestamp.isoformat(),
+        asset=transfer_transaction.asset,
+        exchange=transfer_transaction.to_exchange,
+        holder=transfer_transaction.to_holder,
+        transaction_type=from_in_transaction.transaction_type.value,
+        crypto_in=amount,
+        spot_price=from_in_transaction.spot_price,
+        # TODO: crypto_fee should be split proportionally to the crypto_in.
+        crypto_fee=ZERO,
+        # TODO: initialize fiat fields...
+        row=artificial_id,
+        unique_id=f"{transfer_transaction.unique_id}/{artificial_id}",
+        notes=f"Artificial transaction modeling the reception of {amount} {transfer_transaction.asset} from {transfer_transaction.from_exchange}/{transfer_transaction.from_holder} to {transfer_transaction.to_exchange}/{transfer_transaction.to_holder} on {transfer_transaction.timestamp}.",
+        from_lot=from_in_transaction,
+    )
+    current_transaction: Optional[InTransaction] = result
+    to_account = Account(transfer_transaction.to_exchange, transfer_transaction.to_holder)
+    while True:
+        current_transaction = current_transaction.from_lot if current_transaction is not None else None
+        if current_transaction is None:
+            break
+        current_account = Account(current_transaction.exchange, current_transaction.holder)
+        result.originates_from[current_account] = current_transaction
+        to_lots = current_transaction.to_lots.setdefault(to_account, [])
+        to_lots.append(result)
+
+    return result
+
+
+def _convert_per_wallet_transactions_to_input_data(
+    configuration: Configuration, universal_input_data: InputData, per_wallet_transactions: _PerWalletTransactions
+) -> InputData:
+    in_transaction_set = TransactionSet(configuration, "IN", universal_input_data.asset, universal_input_data.from_date, universal_input_data.to_date)
+    for in_transaction in per_wallet_transactions.in_transactions.acquired_lot_list:
+        in_transaction_set.add_entry(in_transaction)
+    result: InputData = InputData(
+        universal_input_data.asset,
+        in_transaction_set,
+        per_wallet_transactions.out_transactions,
+        per_wallet_transactions.intra_transactions,
+        universal_input_data.from_date,
+        universal_input_data.to_date,
+    )
+    return result
+
+# _process_remaining_transfer_amount processes the remaining amount of a transfer: it handles the cases of a loop, a cycle or a normal transfer.
+# In the last case it creates an artificial InTransaction to model the remaining amount.
+def _process_remaining_transfer_amount(
+    configuration: Configuration,
+    wallet_2_per_wallet_transactions: Dict[Account, _PerWalletTransactions],
+    current_in_lot_and_amount: AcquiredLotAndAmount,
+    transfer: IntraTransaction,
+    remaining_amount: RP2Decimal,
+) -> None:
+    from_account = Account(transfer.from_exchange, transfer.from_holder)
+    from_per_wallet_transactions = wallet_2_per_wallet_transactions[from_account]
+    to_account = Account(transfer.to_exchange, transfer.to_holder)
+    to_per_wallet_transactions = wallet_2_per_wallet_transactions[to_account]
+    if transfer.is_self_transfer():
+        # Transaction to self (loop): do nothing.
+        pass
+    elif to_account in current_in_lot_and_amount.acquired_lot.originates_from:
+        # Transaction cycle detected: the to_account has already been visited. Return the remaining amount to the start of the cycle transaction.
+        start_of_cycle: InTransaction = current_in_lot_and_amount.acquired_lot.originates_from[to_account]
+        start_of_cycle_per_wallet_transactions = to_per_wallet_transactions
+        partial_amount = start_of_cycle_per_wallet_transactions.in_transactions.get_partial_amount(start_of_cycle)
+        if partial_amount + remaining_amount > start_of_cycle.crypto_in:
+            raise RP2ValueError(
+                f"Internal error: start-of-cycle transaction doesn't have enough crypto_in for returned amount: "
+                f"{partial_amount} + {remaining_amount} > {start_of_cycle.crypto_in}: {start_of_cycle}"
+            )
+        start_of_cycle_per_wallet_transactions.in_transactions.set_partial_amount(start_of_cycle, partial_amount + remaining_amount)
+    else:
+        # Normal case: create an artificial InTransaction for the remaining amount and add it to the to_per_wallet_transactions.
+        to_in_transaction = _create_to_in_transaction(configuration, current_in_lot_and_amount.acquired_lot, transfer, remaining_amount)
+        to_per_wallet_transactions.in_transactions.add_acquired_lot(to_in_transaction)
+    # Remove the remaining amount from the partial amount of the current in lot.
+    from_per_wallet_transactions.in_transactions.set_partial_amount(current_in_lot_and_amount.acquired_lot, current_in_lot_and_amount.amount - remaining_amount)
+
+
+# This function performs transfer analysis on an InputData and generates as many new InputData objects as there are wallets.
+# For details see https://github.com/eprbell/rp2/wiki/Adding-Per%E2%80%90Wallet-Application-to-RP2.
+def _transfer_analysis(
+    configuration: Configuration, transferred_lot_selection_method: AbstractAccountingMethod, universal_input_data: InputData
+) -> Dict[Account, InputData]:
+    all_transactions: TransactionSet = TransactionSet(configuration, "MIXED", universal_input_data.asset)
+    for transaction_set in [
+        universal_input_data.unfiltered_in_transaction_set,
+        universal_input_data.unfiltered_out_transaction_set,
+        universal_input_data.unfiltered_intra_transaction_set,
+    ]:
+        for transaction in transaction_set:
+            all_transactions.add_entry(transaction)
+    wallet_2_per_wallet_transactions: Dict[Account, _PerWalletTransactions] = {}
+    for transaction in all_transactions:
+        if isinstance(transaction, InTransaction):
+            account = Account(transaction.exchange, transaction.holder)
+            per_wallet_transactions = wallet_2_per_wallet_transactions.setdefault(
+                account,
+                _PerWalletTransactions(
+                    configuration, universal_input_data.asset, transferred_lot_selection_method, universal_input_data.from_date, universal_input_data.to_date
+                ),
+            )
+            per_wallet_transactions.in_transactions.add_acquired_lot(transaction)
+            per_wallet_transactions.in_transactions.set_to_index(len(per_wallet_transactions.in_transactions.acquired_lot_list) - 1)
+        elif isinstance(transaction, OutTransaction):
+            account = Account(transaction.exchange, transaction.holder)
+            # The wallet transactions object must have been already created when processing a previous InTransaction.
+            per_wallet_transactions = wallet_2_per_wallet_transactions[account]
+            per_wallet_transactions.out_transactions.add_entry(transaction)
+        elif isinstance(transaction, IntraTransaction):
+            # The wallet transactions object must have been already created when processing a previous InTransaction.
+            # IntraTransactions are added to from_per_wallet_transactions.
+            from_account = Account(transaction.from_exchange, transaction.from_holder)
+            from_per_wallet_transactions = wallet_2_per_wallet_transactions[from_account]
+            from_per_wallet_transactions.intra_transactions.add_entry(transaction)
+            to_account = Account(transaction.to_exchange, transaction.to_holder)
+            wallet_2_per_wallet_transactions.setdefault(
+                to_account,
+                _PerWalletTransactions(
+                    configuration, universal_input_data.asset, transferred_lot_selection_method, universal_input_data.from_date, universal_input_data.to_date
+                ),
+            )
+            amount_left_to_transfer = transaction.crypto_received
+            original_partial_amounts: Dict[InTransaction, RP2Decimal] = {}
+            while True:
+                current_in_lot_and_amount = transferred_lot_selection_method.seek_non_exhausted_acquired_lot(
+                    from_per_wallet_transactions.in_transactions, transaction.crypto_received
+                )
+                if current_in_lot_and_amount is None:
+                    raise RP2ValueError(
+                        f"Insufficient balance on {from_account} to send funds (amount {amount_left_to_transfer} {transaction.asset}): {transaction}"
+                    )
+                if current_in_lot_and_amount.amount >= amount_left_to_transfer:
+                    _process_remaining_transfer_amount(
+                        configuration,
+                        wallet_2_per_wallet_transactions,
+                        current_in_lot_and_amount,
+                        transaction,
+                        amount_left_to_transfer,
+                    )
+                    original_partial_amounts[current_in_lot_and_amount.acquired_lot] = current_in_lot_and_amount.amount
+                    if transaction.is_self_transfer():
+                        # self-transfer
+                        for current_transaction, original_partial_amount in original_partial_amounts.items():
+                            from_per_wallet_transactions.in_transactions.set_partial_amount(current_transaction, original_partial_amount)
+                        # TODO: This works only for chronological lot candidates, not for feature based ones.
+                        # reset from_index after reverting partial amounts.
+                        from_per_wallet_transactions.in_transactions.set_from_index(0)
+                    break
+                _process_remaining_transfer_amount(
+                    configuration,
+                    wallet_2_per_wallet_transactions,
+                    current_in_lot_and_amount,
+                    transaction,
+                    current_in_lot_and_amount.amount,
+                )
+                original_partial_amounts[current_in_lot_and_amount.acquired_lot] = current_in_lot_and_amount.amount
+                amount_left_to_transfer -= current_in_lot_and_amount.amount
+        else:
+            raise RP2ValueError(f"Internal error: invalid transaction class: {transaction}")
+
+    # Convert per-wallet transactions to input_data and call the tax engine.
+    result: Dict[Account, InputData] = {}
+    for wallet, per_wallet_transactions in wallet_2_per_wallet_transactions.items():
+        per_wallet_input_data = _convert_per_wallet_transactions_to_input_data(configuration, universal_input_data, per_wallet_transactions)
+        result[wallet] = per_wallet_input_data
+    return result
