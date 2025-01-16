75a1c8d959a8764840f57dfbbc7955d3c3f693b9
eprbell <77937475+eprbell@users.noreply.github.com>
2025-01-15 19:55:12 -0800

    Added actual amount dictionary (for per-wallet application) and moved in create_unfiltered_taxable_event_set()

--- src/rp2/input_data.py
+++ src/rp2/input_data.py
@@ -13,9 +13,14 @@
 # limitations under the License.
 
 from datetime import date
+from typing import Dict, Optional, cast
 
+from rp2.abstract_entry import AbstractEntry
+from rp2.abstract_transaction import AbstractTransaction
 from rp2.configuration import MAX_DATE, MIN_DATE, Configuration
 from rp2.entry_types import EntrySetType
+from rp2.in_transaction import InTransaction
+from rp2.rp2_decimal import RP2Decimal
 from rp2.rp2_error import RP2TypeError
 from rp2.transaction_set import TransactionSet
 
@@ -34,6 +39,7 @@ class InputData:
         unfiltered_in_transaction_set: TransactionSet,
         unfiltered_out_transaction_set: TransactionSet,
         unfiltered_intra_transaction_set: TransactionSet,
+        in_transaction_2_actual_amount: Optional[Dict[InTransaction, RP2Decimal]] = None,
         from_date: date = MIN_DATE,
         to_date: date = MAX_DATE,
     ):
@@ -47,6 +53,9 @@ class InputData:
         self.__unfiltered_intra_transaction_set: TransactionSet = TransactionSet.type_check(
             "intra_transaction_set", unfiltered_intra_transaction_set, EntrySetType.INTRA, self.asset, True
         )
+        self.__in_transaction_2_actual_amount: Dict[InTransaction, RP2Decimal] = (
+            in_transaction_2_actual_amount if in_transaction_2_actual_amount is not None else {}
+        )
         if not isinstance(from_date, date):
             raise RP2TypeError("Parameter 'from_date' is not of type date")
         if not isinstance(to_date, date):
@@ -58,6 +67,23 @@ class InputData:
 
         self.__from_date = from_date
         self.__to_date = to_date
+
+    def create_unfiltered_taxable_event_set(self, configuration: Configuration) -> TransactionSet:
+        transaction_set: TransactionSet
+        entry: AbstractEntry
+        transaction: AbstractTransaction
+        taxable_event_set: TransactionSet = TransactionSet(configuration, "MIXED", self.asset, MIN_DATE, MAX_DATE)
+        for transaction_set in [
+            self.unfiltered_in_transaction_set,
+            self.unfiltered_out_transaction_set,
+            self.unfiltered_intra_transaction_set,
+        ]:
+            for entry in transaction_set:
+                transaction = cast(AbstractTransaction, entry)
+                if transaction.is_taxable():
+                    taxable_event_set.add_entry(transaction)
+
+        return taxable_event_set
 
     @property
     def asset(self) -> str:
@@ -86,6 +112,13 @@ class InputData:
     @property
     def filtered_intra_transaction_set(self) -> TransactionSet:
         return self.__filtered_intra_transaction_set
+
+    # This is non-empty only in per-wallet application: InputData can contain artificial InTransactions that overlap with other InTransactions
+    # (artificial or not). To understand which of these InTransactions actually contain funds and how much this Dict can be used. With universal
+    # application this is always empty.
+    @property
+    def in_transaction_2_actual_amount(self) -> Dict[InTransaction, RP2Decimal]:
+        return self.__in_transaction_2_actual_amount
 
     @property
     def from_date(self) -> date:
