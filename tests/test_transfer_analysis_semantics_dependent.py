f3fdd8092aeecd1a8fd52d831c2721fd9d27f3d1
eprbell <77937475+eprbell@users.noreply.github.com>
2025-02-01 23:51:45 -0800

    Fixed a FIFO test

--- tests/test_transfer_analysis_semantics_dependent.py
+++ tests/test_transfer_analysis_semantics_dependent.py
@@ -189,18 +189,19 @@ class TestTransferAnalysis(AbstractTransferAnalysis):
                     Account("Coinbase", "Bob"): [
                         InTransactionDescriptor("1", 1, 1, "Coinbase", "Bob", 110, 10, to_lot_unique_ids={Account("Kraken", "Bob"): ["3/-1"]}),
                         IntraTransactionDescriptor("3", 3, 3, "Coinbase", "Bob", "Kraken", "Bob", 130, 4, 3),
+                        InTransactionDescriptor("4/-2", 4, -2, "Coinbase", "Bob", 120, 1, from_lot_unique_id="2", cost_basis_day=2),
                         OutTransactionDescriptor("6", 6, 6, "Coinbase", "Bob", 150, 3, 2),
                     ],
                     Account("Kraken", "Bob"): [
                         InTransactionDescriptor("3/-1", 3, -1, "Kraken", "Bob", 110, 3, from_lot_unique_id="1", cost_basis_day=1),
-                        InTransactionDescriptor("2", 2, 2, "Kraken", "Bob", 120, 10),
+                        InTransactionDescriptor("2", 2, 2, "Kraken", "Bob", 120, 10, to_lot_unique_ids={Account("Coinbase", "Bob"): ["4/-2"]}),
                         IntraTransactionDescriptor("4", 4, 4, "Kraken", "Bob", "Coinbase", "Bob", 140, 2, 1),
                         OutTransactionDescriptor("5", 5, 5, "Kraken", "Bob", 150, 2, 1),
                     ],
                 },
                 want_amounts={
-                    Account(exchange='Coinbase', holder='Bob'): {'1': 2},
-                    Account(exchange='Kraken', holder='Bob'): {'2': 8, '3/-1': 0},
+                    Account(exchange='Coinbase', holder='Bob'): {'1': 1},
+                    Account(exchange='Kraken', holder='Bob'): {'2': 5},
                 },
                 want_error="",
             ),
